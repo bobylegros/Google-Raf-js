2a8fc35897614b5004c5565a67dc4f95162a0a0d
bobylegros <rafcapdeville@gmail.com>
2018-01-11 17:40:07 +0100

    Sprit intermed2

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,8 @@
 function toto(){
   alert("Top de la balle!!!");
   document.getElementById("section_button").style.display = "none";
+
+}
+function prout(){
+  document.getElementById("section_button").style.display = "block";
 }
