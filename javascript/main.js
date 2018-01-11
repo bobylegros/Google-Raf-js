e5a79d0d15636a7eb136160d38fef4ef97b489a6
bobylegros <rafcapdeville@gmail.com>
2018-01-11 19:35:42 +0100

    Sprit intermed3

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,26 @@
-function toto(){
-  alert("Top de la balle!!!");
+function hideSection(){
+  alert("hideSection()> debug");
+  document.getElementById("section_button").style.display = "block";
+  document.getElementById("panel").style.display = "none";
+}
+
+
+function showSection(){
   document.getElementById("section_button").style.display = "none";
+  document.getElementById("panel").style.display = "block";
 
 }
-function prout(){
-  document.getElementById("section_button").style.display = "block";
+
+function jaiDeLaChance(){
+  let text = "J'ai de la chance!!!"
+  alert(text);
+  text = text.replace(/ /g, "+");
+  alert(text);
+}
+
+function updateInput(){
+  let text = document.getElementById("google").value;
+  alert(text);
+  text = text.replace(/ /g, "+");
+  alert(text);
 }
--- scripts/main.js
+++
@@ -1,3 +0,0 @@
-function toto(){
-  alert("Top de la balle!!!");
-}
